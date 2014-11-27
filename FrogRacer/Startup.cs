b56ef5d739ad93397e2cabbf74ee4ce65383a577
Firebird82 <david.sebela@gmail.com>
2014-11-27 11:04:59 +0100

    Nytt MVC projekt

---
+++ FrogRacer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrogRacer.Startup))]
+namespace FrogRacer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
