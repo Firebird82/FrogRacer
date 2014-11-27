d6acadbe81f8941e5cd78b6e9fefdda4ca778f51
Firebird82 <david.sebela@gmail.com>
2014-11-27 15:17:48 +0100

    User klass

---
+++ WorkerRole/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerRole
+{
+    class User
+    {
+        public string UserName { get; set; }
+        public int Balance { get; set; }
+    }
+}
