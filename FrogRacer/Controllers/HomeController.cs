fc96b7d97dd76b07709b99c923ee8ca961de2bea
Firebird82 <david.sebela@gmail.com>
2014-11-28 10:51:47 +0100

    Möblerat om i Signup

--- FrogRacer/Controllers/HomeController.cs
+++ FrogRacer/Controllers/HomeController.cs
@@ -27,42 +27,42 @@ namespace FrogRacer.Controllers
         {
             User _user = new User(userName);
 
-     
-            if (userName != null)
+            if (string.IsNullOrEmpty(userName))
+            {
+                return View("SignUp");
+            }
+            else
             {
                 Session["UserName"] = userName;
 
                 string user = (string)Session["UserName"];
+
+                var nm = NamespaceManager.CreateFromConnectionString(connectionString);
+                QueueDescription qd = new QueueDescription(qname);
+                //Ställ in Max size på queue på  2GB
+                qd.MaxSizeInMegabytes = 2048;
+                //Max Time To Live är 5 minuter  
+                qd.DefaultMessageTimeToLive = new TimeSpan(0, 5, 0);
+
+                if (!nm.QueueExists(qname))
+                {
+                    nm.CreateQueue(qd);
+                }
+
+                //Skicka till queue med hjälp av den connectionstring vi tidigare ställt in i configen
+                QueueClient qc = QueueClient.CreateFromConnectionString(connectionString, qname);
+
+                //Skapa msg med email properaty och skicka till QueueClient
+                var bm = new BrokeredMessage();
+                bm.Properties["userName"] = _user.UserName;
+                bm.Properties["balance"] = _user.Balance;
+                qc.Send(bm);
+
+                ViewBag.message = "Hej " + _user.UserName + ".Du har ett välkomstslado på " + _user.Balance;
+
+                return View("Betting");
             }
-            else
-            {
-                return View("SignUp");
-            }
 
-            var nm = NamespaceManager.CreateFromConnectionString(connectionString);
-            QueueDescription qd = new QueueDescription(qname);
-            //Ställ in Max size på queue på  2GB
-            qd.MaxSizeInMegabytes = 2048;
-            //Max Time To Live är 5 minuter  
-            qd.DefaultMessageTimeToLive = new TimeSpan(0, 5, 0);
-
-            if (!nm.QueueExists(qname))
-            {
-                nm.CreateQueue(qd);
-            }
-
-            //Skicka till queue med hjälp av den connectionstring vi tidigare ställt in i configen
-            QueueClient qc = QueueClient.CreateFromConnectionString(connectionString, qname);
-
-            //Skapa msg med email properaty och skicka till QueueClient
-            var bm = new BrokeredMessage();
-            bm.Properties["userName"] = _user.UserName;
-            bm.Properties["balance"] = _user.Balance;
-            qc.Send(bm);
-
-            ViewBag.message ="Hej " + _user.UserName + ".Du har ett välkomstslado på "+ _user.Balance;
-
-            return View("Betting");
         }
     }
 }
